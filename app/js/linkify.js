cc899d00e09914524f0b2b5d6fa0c87c1d3f5228
ENT8R <info.ent8r@gmail.com>
2021-05-31 14:08:10 +0200

    :computer: Display more images (fix #65)

--- app/js/linkify.js
+++ app/js/linkify.js
@@ -1,17 +1,20 @@
 import anchorme from 'anchorme';
 
 const IMAGE_HOSTING_REGEX = {
-  imgur: /(http(s)?:\/\/)?i\.imgur\.com\/\w+\.(jpg|png)/gi,
-  framapic: /(http(s)?:\/\/)?(www\.)?framapic\.org\/(random\?i=)?\w+\/\w+(\.(jpg|jpeg|png))?/gi,
-  westnordost: /https:\/\/westnordost\.de\/p\/[0-9]+\.jpg/gi,
-  wikimedia: /http(?:s)?:\/\/upload\.wikimedia\.org\/wikipedia\/(.+?)\/(?:thumb\/)?(\w\/\w\w)\/(.+?\.(?:jpg|jpeg|png))(?:\/.+?\.(?:jpg|jpeg|png))?/gi,
-  commons: /http(?:s)?:\/\/commons\.wikimedia\.org\/wiki\/File:(.+?\.(?:jpg|jpeg|png|svg))/gi,
-  mapillary: /http(?:s)?:\/\/(?:www\.)?mapillary\.com\/map\/im\/(\w+)/gi
+  imgur: /(http(s)?:\/\/)?(i\.)?imgur\.com\/\w+\.(jpg|png)/i,
+  framapic: /(http(s)?:\/\/)?(www\.)?framapic\.org\/(random\?i=)?\w+\/\w+(\.(jpg|jpeg|png))?/i,
+  westnordost: /https:\/\/westnordost\.de\/p\/[0-9]+\.jpg/i,
+  wikimedia: /http(?:s)?:\/\/upload\.wikimedia\.org\/wikipedia\/(.+?)\/(?:thumb\/)?(\w\/\w\w)\/(.+?\.(?:jpg|jpeg|png))(?:\/.+?\.(?:jpg|jpeg|png))?/i,
+  commons: /http(?:s)?:\/\/commons\.wikimedia\.org\/wiki\/File:(.+?\.(?:jpg|jpeg|png|svg))/i,
+  openstreetmap: /http(?:s)?:\/\/wiki\.openstreetmap\.org\/wiki\/File:(.+?\.(?:jpg|jpeg|png|svg))/i,
+  mapillary: /http(?:s)?:\/\/(?:www\.)?mapillary\.com\/map\/im\/(\w+)/i,
+  all: /(http(s)?:\/\/)?(www\.)?.+\.(jpg|jpeg|png)/i
 };
 
 const IMAGE_HOSTING_ADDITIONAL_FORMATTING = {
   wikimedia: 'https://upload.wikimedia.org/wikipedia/$1/thumb/$2/$3/300px-$3',
   commons: 'https://commons.wikimedia.org/wiki/Special:FilePath/$1?width=300',
+  openstreetmap: 'https://wiki.openstreetmap.org/wiki/Special:FilePath/$1?width=300',
   mapillary: 'https://images.mapillary.com/$1/thumb-320.jpg'
 };
 
@@ -26,7 +29,7 @@ export default function replace(input) {
   input = escape(input); // Sanitize the input
 
   const images = [];
-  const extensions = Object.entries(IMAGE_HOSTING_REGEX).map(([ provider, regex ]) => {
+  const specialTransform = Object.entries(IMAGE_HOSTING_REGEX).map(([ provider, regex ]) => {
     return {
       test: regex,
       transform: url => {
@@ -48,8 +51,8 @@ export default function replace(input) {
         target: '_blank',
         rel: 'noopener noreferrer'
       },
-    },
-    extensions
+      specialTransform
+    }
   }).replace(/\r?\n/g, '<br>') // 1. Replace all newlines with the corresponding HTML element
     .replace(/(\/a>)(<br>)(<a)/g, '$1$3'); // 2. Remove all line breaks between multiple images
 
