c2cfe7cd413ae170bbe3c7980ef589862fd6c16d
ENT8R <info.ent8r@gmail.com>
2023-10-28 22:14:16 +0200

    :arrow_up: Update dependencies

--- app/js/polyfills.js
+++ app/js/polyfills.js
@@ -1,5 +1,3 @@
 // chrome >= 66, edge >= 16, firefox >= 57, not ie <= 11, opera >= 53, safari >= 11.1
 import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
 
-// Needed because of https://github.com/handlebars-lang/handlebars.js/pull/1692
-window.global = window;
