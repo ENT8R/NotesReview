e7be83a37c26e493d1bc411df1be8594208bd299
ENT8R <info.ent8r@gmail.com>
2026-06-13 20:50:30 +0200

    :dizzy: Animate transitions of the SingleSelectionButtonGroup

--- app/js/elements/SingleSelectionButtonGroup.js
+++ app/js/elements/SingleSelectionButtonGroup.js
@@ -6,14 +6,25 @@ export default class SingleSelectionButtonGroup extends HTMLElement {
     super();
     this._internals = this.attachInternals();
 
-    this.activeButton = this.querySelector('button.active');
+    this.currentButton = this.querySelector('button.active');
+
+    // Create a new selection indicator that will be used to highlight the currently selected button
+    this.selectionIndicator = document.createElement('div');
+    this.selectionIndicator.classList.add('selection-indicator');
+    this.prepend(this.selectionIndicator);
   }
 
   connectedCallback() {
     this.querySelectorAll('button').forEach(button => {
       button.addEventListener('click', this.#onClick.bind(this));
     });
-    this.value = this.activeButton.dataset.value || null;
+    this.value = this.currentButton.dataset.value || null;
+
+    // Use a ResizeObserver to update the indicator as well when the button group is resized
+    this.resizeObserver = new ResizeObserver(() => {
+      this.updateIndicator();
+    });
+    this.resizeObserver.observe(this);
   }
 
   formStateRestoreCallback(state) {
@@ -40,14 +51,26 @@ export default class SingleSelectionButtonGroup extends HTMLElement {
     }
 
     // Reset the currently active button
-    this.activeButton.classList.remove('active');
+    this.currentButton.classList.remove('active');
 
     // Set the next button to an active state
-    this.activeButton = nextButton;
-    this.activeButton.classList.add('active');
+    this.currentButton = nextButton;
+    this.currentButton.classList.add('active');
+    this.updateIndicator();
 
     // Set the new value internally and update the form value
     this._v = v;
     this._internals.setFormValue(this._v);
+  }
+
+  updateIndicator() {
+    if (!this.currentButton || this.currentButton.offsetWidth === 0 || this.currentButton.offsetHeight === 0) {
+      return;
+    }
+
+    this.selectionIndicator.style.left = `${this.currentButton.offsetLeft}px`;
+    this.selectionIndicator.style.top = `${this.currentButton.offsetTop}px`;
+    this.selectionIndicator.style.width = `${this.currentButton.offsetWidth}px`;
+    this.selectionIndicator.style.height = `${this.currentButton.offsetHeight}px`;
   }
 }
